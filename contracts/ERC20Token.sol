3918c18497fef4e7f7282981ad828ede2d93bb0d
tashun <tashun.chamara@gmail.com>
2023-02-05 23:20:16 +0530

    erc20 token

---
+++ contracts/ERC20Token.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.9;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./BaseContract.sol";
+
+contract ERC20Token is ERC20, BaseContract {
+    function initialize(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
+        _mint(address(this), initialSupply * 10 ** decimals());
+    }
+
+    function mintExtraTokens(uint256 addingSupply) external onlyRole(ADMIN_ROLE) {
+        _mint(msg.sender, addingSupply * 10 ** decimals());
+    }
+
+    function transferUserNewlyCreatedTokens(address _address, uint256 amount) external onlyRole(ADMIN_ROLE) {
+        _mint(msg.sender, amount * 10 ** decimals());
+        _transfer(msg.sender, _address, amount * 10 ** decimals());
+    }
+
+
+}
