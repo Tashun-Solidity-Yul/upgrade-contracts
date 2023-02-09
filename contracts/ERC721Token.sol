43ae6dfb6469cc4e82cc5173d9b3ad052393a9d8
tashun <tashun.chamara@gmail.com>
2023-02-09 09:03:21 +0530

    temp changes

--- contracts/ERC721Token.sol
+++ contracts/ERC721Token.sol
@@ -1,25 +1,27 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.9;
 
-import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
 import "./BaseContract.sol";
 import "./ERC20Token.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 
-contract ERC721Token is BaseContract, ERC721URIStorageUpgradeable {
+contract ERC721Token is BaseContract, ERC721URIStorageUpgradeable, UUPSUpgradeable {
     mapping(address => uint256) private timeLockMap;
 
-    function initialize(string memory name_, string memory symbol_) {
+    function initialize(string memory name_, string memory symbol_) external initializer {
         __ERC721_init(name_,symbol_);
          __ERC721URIStorage_init();
     }
 
-    function supportsInterface(bytes4 interfaceId) public view override(AccessControl, ERC721) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view override(AccessControlUpgradeable, ERC721Upgradeable) returns (bool) {
         return
-        interfaceId == type(IAccessControl).interfaceId ||
-        interfaceId == type(IERC721).interfaceId ||
+        interfaceId == type(IAccessControlUpgradeable).interfaceId ||
+        interfaceId == type(IERC721Upgradeable).interfaceId ||
         super.supportsInterface(interfaceId);
     }
 
@@ -35,13 +37,17 @@ contract ERC721Token is BaseContract, ERC721URIStorageUpgradeable {
         uint256 selectedURIId = getRandomUint256(getTokenId()) % uniqueTokenCount;
         timeLockMap[receiver] = block.timestamp + secondsForADay;
         incrementTokenId();
-        _setTokenURI(getTokenId(), string(abi.encodePacked(uriBaseLocation, Strings.toString(selectedURIId))));
+        _setTokenURI(getTokenId(), string(abi.encodePacked(uriBaseLocation, StringsUpgradeable.toString(selectedURIId))));
         _safeMint(receiver, getTokenId());
     }
 
 
     function getRandomUint256(uint256 input) private view returns (uint256) {
         return uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp, input)));
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal virtual override {
+//        require("");
     }
 
 }
