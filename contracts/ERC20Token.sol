5e1c6c0c914f41235cfe908365776315bbafd967
tashun <tashun.chamara@gmail.com>
2023-02-24 16:50:32 +0530

    ERC20Token

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -1,20 +1,35 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.9;
+pragma solidity 0.8.17;
 
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "./BaseContract.sol";
 
 contract ERC20Token is ERC20Upgradeable, BaseContract {
-    function initialize(string memory name, string memory symbol, uint256 initialSupply) external initializer {
-        _mint(address(this), initialSupply * 10 ** decimals());
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
     }
 
-    function mintExtraTokens(uint256 addingSupply) external onlyRole(ADMIN_ROLE) {
+    function initialize(string memory name, string memory symbol, address ownerAddress, uint256 initialSupply) external initializer {
+        __ERC20_init(name, symbol);
+
+        _setRoleAdmin(OWNER_ROLE, OWNER_ROLE);
+        _setRoleAdmin(EXECUTIVE_ROLE, EXECUTIVE_ROLE);
+        _setupRole(OWNER_ROLE, ownerAddress);
+        _mint(ownerAddress, initialSupply * 10 ** decimals());
+
+    }
+
+    function setupExecutiveRole(address executiveAddress) external onlyRole(OWNER_ROLE) {
+        _setupRole(EXECUTIVE_ROLE, executiveAddress);
+    }
+
+    function mintExtraTokens(uint256 addingSupply) external onlyRole(EXECUTIVE_ROLE) {
         _mint(msg.sender, addingSupply * 10 ** decimals());
     }
 
-    function transferUserNewlyCreatedTokens(address _address, uint256 amount) external onlyRole(ADMIN_ROLE) {
+    function transferUserNewlyCreatedTokens(address _address, uint256 amount) external onlyRole(EXECUTIVE_ROLE) {
         _mint(msg.sender, amount * 10 ** decimals());
         _transfer(msg.sender, _address, amount * 10 ** decimals());
     }
