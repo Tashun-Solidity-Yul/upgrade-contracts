102f195463553ac5a4595c3a97841f57d4bec28f
tashun <tashun.chamara@gmail.com>
2023-03-02 02:52:11 +0530

    clone test, uups and transparent proxy

--- test/Authoritative.js
+++ test/Authoritative.js
@@ -14,7 +14,15 @@ describe('Authoritative Token Tests', function () {
     const INITIAL_ATTACKER_TOKEN_BALANCE = ethers.utils.parseEther('100')
 
     before(async function () {
-        ;[deployer, alice, bob] = await ethers.getSigners()
+        [deployer, alice, bob] = await ethers.getSigners()
+        console.log(alice.address)
+
+
+
+
+    })
+
+    it('Transparent Proxy Deployment Test', async function () {
         const {
             ERC20Token,
             erc20ContractProxy,
@@ -22,7 +30,8 @@ describe('Authoritative Token Tests', function () {
             erc721ContractProxy,
             AuthoritativeToken,
             authoritativeTokenProxy,
-        } = await loadFixture(deployContracts)
+            ERC721WithGodMod
+        } = await loadFixture(deployContractsWithTransparentProxyPattern)
 
         const erc721ContractProxyWithABISupport = await ERC721Token.attach(
             erc721ContractProxy.address,
@@ -33,17 +42,21 @@ describe('Authoritative Token Tests', function () {
         const authoritativeContractProxyWithABISupport = await AuthoritativeToken.attach(
             authoritativeTokenProxy.address,
         )
-
+        // ------------------------------------------------------
         const tnx1 = await erc721ContractProxyWithABISupport
             .connect(deployer)
             .setupExecutiveRole(authoritativeTokenProxy.address)
         await tnx1.wait()
+
+        // ------------------------------------------------------
+
         const tnx2 = await erc20ContractProxyWithABISupport
             .connect(deployer)
             .setupExecutiveRole(authoritativeTokenProxy.address)
         await tnx2.wait()
 
         console.log(await erc721ContractProxyWithABISupport.OWNER_ROLE())
+
         console.log(
             await erc721ContractProxyWithABISupport.getRoleAdmin(
                 await authoritativeContractProxyWithABISupport.OWNER_ROLE(),
@@ -55,43 +68,145 @@ describe('Authoritative Token Tests', function () {
                 deployer.address,
             ),
         )
+        // ------------------------------------------------------
         // await erc721ContractProxyWithABISupport.mintNewNFTThroughContract(deployer.address)
         const tnx3 = await erc20ContractProxyWithABISupport
             .connect(deployer)
             .approve(authoritativeTokenProxy.address, 10n ** 17n)
         await tnx3.wait()
+
+        // ------------------------------------------------------
         const tnx4 = await authoritativeContractProxyWithABISupport
             .connect(deployer)
             .mintNFT()
         await tnx4.wait()
+
+        // ------------------------------------------------------
         await expect(
             erc721ContractProxyWithABISupport
                 .connect(deployer)
                 .mintNewNFTThroughContract(deployer.address),
         ).to.be.reverted
-
+        // ------------------------------------------------------
         const tnx5 = await erc721ContractProxyWithABISupport.connect(deployer).approve(authoritativeContractProxyWithABISupport.address, 1)
         await tnx5.wait()
+
+        // ------------------------------------------------------
         const tnx6 = await authoritativeContractProxyWithABISupport.depositNFT(1)
         await tnx6.wait()
+
+        // ------------------------------------------------------
+
+        const eRC721WithGodMod = await upgrades.upgradeProxy(erc721ContractProxy.address, ERC721WithGodMod);
+        await eRC721WithGodMod.deployed();
+        console.log("eRC721WithGodMod : " + eRC721WithGodMod.address)
+
+        // ------------------------------------------------------
+        const tnx7 = await authoritativeTokenProxy.connect(deployer).withdrawNFT(1)
+        await tnx7.wait();
+
+        // ------------------------------------------------------
+        const tnx8 = await eRC721WithGodMod.connect(deployer).transferNFTs(deployer.address,alice.address,1);
+        await tnx8.wait();
+        // ------------------------------------------------------
+        await expect(authoritativeTokenProxy.connect(deployer).withdrawNFT(1)).to.be.revertedWithCustomError(authoritativeTokenProxy,"TokenNotDeposited")
+
     })
 
-    it('Exploit', async function () {
-        // const erc721ContractProxy = '0xaEB84da89F8654Be192FA59289Ba6BbFa340a71E'
-        // const ERC721WithGodMod = await ethers.getContractFactory('ERC721WithGodMod')
 
-        // const eRC721WithGodMod = await upgrades.upgradeProxy(
-        //     erc721ContractProxy.address,
-        //     ERC721WithGodMod,
-        // )
-        // await eRC721WithGodMod.deployed()
+    it('UUPS Proxy Deployment Test', async function () {
+        const {
+            ERC20TokenV2,
+            erc20ContractProxyV2,
+            ERC721TokenV2,
+            erc721ContractProxyV2,
+            AuthoritativeTokenV2,
+            authoritativeTokenProxyV2,
+            ERC721WithGodModV2
+        } = await loadFixture(deployContractsWithUUPSProxyPattern)
+
+        const erc721ContractProxyV2WithABISupport = await ERC721TokenV2.attach(
+            erc721ContractProxyV2.address,
+        )
+        const erc20ContractProxyV2WithABISupport = await ERC20TokenV2.attach(
+            erc20ContractProxyV2.address,
+        )
+        const authoritativeContractProxyV2WithABISupport = await AuthoritativeTokenV2.attach(
+            authoritativeTokenProxyV2.address,
+        )
+        // ------------------------------------------------------
+        const tnx1 = await erc721ContractProxyV2WithABISupport
+            .connect(deployer)
+            .setupExecutiveRole(authoritativeTokenProxyV2.address)
+        await tnx1.wait()
+
+        // ------------------------------------------------------
+
+        const tnx2 = await erc20ContractProxyV2WithABISupport
+            .connect(deployer)
+            .setupExecutiveRole(authoritativeTokenProxyV2.address)
+        await tnx2.wait()
+
+        console.log(await erc721ContractProxyV2WithABISupport.OWNER_ROLE())
+
+        console.log(
+            await erc721ContractProxyV2WithABISupport.getRoleAdmin(
+                await authoritativeContractProxyV2WithABISupport.OWNER_ROLE(),
+            ),
+        )
+        console.log(
+            await erc721ContractProxyV2WithABISupport.hasRole(
+                await authoritativeContractProxyV2WithABISupport.OWNER_ROLE(),
+                deployer.address,
+            ),
+        )
+        // ------------------------------------------------------
+        // await erc721ContractProxyWithABISupport.mintNewNFTThroughContract(deployer.address)
+        const tnx3 = await erc20ContractProxyV2WithABISupport
+            .connect(deployer)
+            .approve(authoritativeTokenProxyV2.address, 10n ** 17n)
+        await tnx3.wait()
+
+        // ------------------------------------------------------
+        const tnx4 = await authoritativeContractProxyV2WithABISupport
+            .connect(deployer)
+            .mintNFT()
+        await tnx4.wait()
+
+        // ------------------------------------------------------
+        await expect(
+            erc721ContractProxyV2WithABISupport
+                .connect(deployer)
+                .mintNewNFTThroughContract(deployer.address),
+        ).to.be.reverted
+        // ------------------------------------------------------
+        const tnx5 = await erc721ContractProxyV2WithABISupport.connect(deployer).approve(authoritativeContractProxyV2WithABISupport.address, 1)
+        await tnx5.wait()
+
+        // ------------------------------------------------------
+        const tnx6 = await authoritativeContractProxyV2WithABISupport.depositNFT(1)
+        await tnx6.wait()
+
+        // ------------------------------------------------------
+
+        const eRC721WithGodModV2 = await upgrades.upgradeProxy(erc721ContractProxyV2.address, ERC721WithGodModV2);
+        await eRC721WithGodModV2.deployed();
+        console.log("eRC721WithGodMod : " + eRC721WithGodModV2.address)
+
+        // ------------------------------------------------------
+        const tnx7 = await authoritativeTokenProxyV2.connect(deployer).withdrawNFT(1)
+        await tnx7.wait();
+
+        // ------------------------------------------------------
+        const tnx8 = await eRC721WithGodModV2.connect(deployer).transferNFTs(deployer.address,"0x70997970C51812dc3A010C7d01b50e0d17dc79C8",1);
+        await tnx8.wait();
+        // ------------------------------------------------------
+        await expect(authoritativeTokenProxyV2.connect(deployer).withdrawNFT(1)).to.be.revertedWithCustomError(authoritativeTokenProxyV2,"TokenNotDeposited")
+
+        const tnx9 = await eRC721WithGodModV2.connect(deployer).transferNFTs("0x70997970C51812dc3A010C7d01b50e0d17dc79C8",deployer.address,1);
+        await tnx9.wait();
     })
-
-    after(async function () { })
-
-    async function getERC20Proxy() { }
-    async function getERC721Proxy() { }
-    async function deployContracts() {
+    async function deployContractsWithTransparentProxyPattern() {
         const ERC20Token = await hre.ethers.getContractFactory(
             'ERC20Token',
             deployer,
@@ -135,6 +250,10 @@ describe('Authoritative Token Tests', function () {
         )
         await authoritativeTokenProxy.deployed()
         console.log('authoritativeToken : ' + authoritativeTokenProxy.address)
+        // const erc721ContractProxy = "0xA734Da4C28D5ED383C0f6e9Fd152B7DCeC576837";
+        const ERC721WithGodMod = await ethers.getContractFactory("ERC721WithGodMod");
+
+
 
         return {
             ERC20Token,
@@ -143,6 +262,68 @@ describe('Authoritative Token Tests', function () {
             erc721ContractProxy,
             AuthoritativeToken,
             authoritativeTokenProxy,
+            ERC721WithGodMod,
+        }
+    }
+
+    async function deployContractsWithUUPSProxyPattern() {
+        const ERC20TokenV2 = await hre.ethers.getContractFactory(
+            'ERC20TokenV2',
+            deployer,
+        )
+        const erc20ContractProxyV2 = await hre.upgrades.deployProxy(
+            ERC20TokenV2,
+            ['DUX', 'DUX', deployer.address, 100000],
+            {
+                initializer: 'initialize',
+            },
+        )
+        await erc20ContractProxyV2.deployed()
+        console.log('erc20ContractProxyV2 : ' + erc20ContractProxyV2.address)
+
+        const ERC721TokenV2 = await ethers.getContractFactory('ERC721TokenV2', deployer)
+        const erc721ContractProxyV2 = await upgrades.deployProxy(
+            ERC721TokenV2,
+            ['GAME', 'GAM', deployer.address],
+            {
+                initializer: 'initialize',
+                kind: "uups"
+            }
+        )
+        await erc721ContractProxyV2.deployed()
+
+        console.log('erc721ContractProxyV2 : ' + erc721ContractProxyV2.address)
+
+        const AuthoritativeTokenV2 = await ethers.getContractFactory(
+            'AuthoritativeTokenV2',
+            deployer,
+        )
+        const authoritativeTokenProxyV2 = await upgrades.deployProxy(
+            AuthoritativeTokenV2,
+            [
+                erc721ContractProxyV2.address,
+                erc20ContractProxyV2.address,
+                deployer.address,
+            ],
+            {
+                initializer: 'initialize',
+            },
+        )
+        await authoritativeTokenProxyV2.deployed()
+        console.log('authoritativeTokenV2 : ' + authoritativeTokenProxyV2.address)
+        // const erc721ContractProxy = "0xA734Da4C28D5ED383C0f6e9Fd152B7DCeC576837";
+        const ERC721WithGodModV2 = await ethers.getContractFactory("ERC721WithGodModV2");
+
+
+
+        return {
+            ERC20TokenV2,
+            erc20ContractProxyV2,
+            ERC721TokenV2,
+            erc721ContractProxyV2,
+            AuthoritativeTokenV2,
+            authoritativeTokenProxyV2,
+            ERC721WithGodModV2,
         }
     }
 })
--- test/CloneLogicTestV2.js
+++ test/CloneLogicTestV2.js
@@ -6,28 +6,33 @@ const {
 } = require('@openzeppelin/hardhat-upgrades/dist/utils')
 const {loadFixture} = require('@nomicfoundation/hardhat-network-helpers')
 
-describe('Clone Logic Test with no constructor', function () {
+describe('Clone Logic Test with constructor', function () {
     let deployer, alice, bob
 
+    // Pool has 1M * 10**18 tokens
+    const TOKENS_IN_POOL = ethers.utils.parseEther('1000000')
+    const INITIAL_ATTACKER_TOKEN_BALANCE = ethers.utils.parseEther('100')
 
     it('Exploit', async function () {
         [deployer, alice, bob] = await ethers.getSigners();
-        const BaseContract = await hre.ethers.getContractFactory("ECR20BaseTokenV1")
+        const BaseContract = await hre.ethers.getContractFactory("ECR20BaseTokenV2")
         const contractInstance = await BaseContract.deploy();
 
-        const FactoryBaseContract = await hre.ethers.getContractFactory("ERC20CloneFactory")
+        const FactoryBaseContract = await hre.ethers.getContractFactory("ERC20CloneFactoryV2")
         const factoryContractInstance = await FactoryBaseContract.deploy(contractInstance.address);
-        //
-        const tnx1 = await factoryContractInstance.createClone()
+
+        const tnx1 = await factoryContractInstance.createClone(40000n * 10n * 18n)
         const receipt = await tnx1.wait();
         const firstEvent = (receipt.events.filter((event) => {
             return event?.topics[0] === '0x1a0f921ce3c6f2f0f6be5b624a487bc1d5143e1fd1833154f39ab63e13d89755'
         }))[0]
 
         const clonedContract = await BaseContract.attach(firstEvent.args[0]);
-        await clonedContract.initialize("FGF","DFG",1000n*10n*18n)
-        expect(await contractInstance.name()).to.be.equal("")
-        expect(await clonedContract.name()).to.be.equal("FGF")
+        await clonedContract.initialize("FGF","DFG")
+        expect(await contractInstance.getConstantSupply()).to.be.equal(0);
+        expect(await clonedContract.getConstantSupply()).to.be.equal(40000n * 10n * 18n);
+
+
 
     })
 })
